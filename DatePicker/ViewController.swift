c6eecba66874ab01737c0f9d99be776496978852
evgenStarikov <evgen@globalbit.co.il>
2015-12-08 18:09:26 +0200

    Initial Commit

---
+++ DatePicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DatePicker
+//
+//  Created by Starikov Evgen on 08.12.15.
+//  Copyright © 2015 Starikov Evgen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
